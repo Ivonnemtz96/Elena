75639b54ae8db89e6cfa55594dbafb5c12c77bf8
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-16 12:58:02 -0700

    Footer Ok

--- includes/footer.php
+++ includes/footer.php
@@ -2,33 +2,32 @@
     <div class="footer-bottom">
         <div class="row text-center h-100 justify-content-center align-items-center">
             <div class="col-md-6">
-                <p class="copyright-text"><i class="ri-copyright-line"></i><span>Duch</span>. All Rights
-                    Reserved By <a href="https://hibootstrap.com/" target="_blank">HiBootstrap</a></p>
+                <p class="copyright-text"><i class="ri-copyright-line"></i><span>Dra. Gloria Elena Lomeli</span>. Diseñado por <a href="https://www.bananagroup.mx/" target="_blank">Banana Group Marketing</a></p>
             </div>
             <div class="col-md-6 text-md-center">
-                <div class="social-link">
-                    <h6>Follow Us On: </h6>
+                <div class="social-link" style="justify-content: center;">
+                    <h6><b>Síguenos en nuestras redes:</b></h6>
                     <ul class="social-profile list-style style1">
                         <li>
                             <a target="_blank" href="https://facebook.com/">
                                 <i class="ri-facebook-line"></i>
                             </a>
                         </li>
-                        <li>
+                        <!-- <li>
                             <a target="_blank" href="https://twitter.com/">
                                 <i class="ri-twitter-line"></i>
                             </a>
-                        </li>
+                        </li> -->
                         <li>
                             <a target="_blank" href="https://instagram.com/">
                                 <i class="ri-instagram-line"></i>
                             </a>
                         </li>
-                        <li>
+                        <!-- <li>
                             <a target="_blank" href="https://linkedin.com/">
                                 <i class="ri-linkedin-line"></i>
                             </a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
