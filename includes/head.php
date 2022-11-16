ebc08f8d918c3da6f242bafbaa3fba07331108fc
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-16 09:23:03 -0700

    Se agrega el head, preloader y header

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/flaticon.css">
+<link rel="stylesheet" href="assets/css/remixicon.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/fancybox.css">
+<link rel="stylesheet" href="assets/css/aos.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+<link rel="stylesheet" href="assets/css/dark-theme.css">
+<title>Dra. Grloria Elena Lomeli</title>
+<link rel="icon" type="image/png" href="assets/img/favicon.png">
+</head>
---
+++ includes/header.php
@@ -0,0 +1,97 @@
+<header class="header-wrap style1">
+    <div class="header-top">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-8 col-md-7">
+                    <div class="header-top-left">
+                        <ul class="contact-info list-style">
+                            <li><i class="flaticon-phone-call"></i> <a href="tel:02459271449">(+024) 592 71 449</a></li>
+                            <li><i class="flaticon-time"></i>
+                                <p>Sat - Thu: 10:00 am - 7:00 pm</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-5">
+                    <div class="header-top-right">
+                        <div class="select-lang">
+                            <i class="ri-global-line"></i>
+                            <div class="navbar-option-item navbar-language dropdown language-option">
+                                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <span class="lang-name"></span>
+                                </button>
+                                <div class="dropdown-menu language-dropdown-menu">
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/img/uk.png" alt="flag">
+                                        English
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/img/china.png" alt="flag">
+                                        简体中文
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/img/uae.png" alt="flag">
+                                        العربيّة
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="social-profile list-style style1">
+                            <li>
+                                <a target="_blank" href="https://facebook.com/">
+                                    <i class="ri-facebook-line"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="https://twitter.com/">
+                                    <i class="ri-twitter-line"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="https://instagram.com/">
+                                    <i class="ri-instagram-line"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a target="_blank" href="https://linkedin.com/">
+                                    <i class="ri-linkedin-line"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="header-bottom">
+            <nav class="navbar navbar-expand-lg navbar-light header-sticky">
+                <a class="navbar-brand" href="index.html">
+                    <img class="logo-light" src="assets/img/logo.png" alt="Image">
+                    <img class="logo-dark" src="assets/img/logo-white.png" alt="Image"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item"><a class="nav-link active" href="#hero">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#service">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#gallery">Gallery</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#team">Doctor</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#pricing">Pricing</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#blog">Blog</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link d-lg-none" href="#appointment">Get Free Quote</a></li>
+                    </ul>
+                </div>
+                <div class="other-option md-none">
+                    <a href="#appointment" class="btn style1">Get Free Quote</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
